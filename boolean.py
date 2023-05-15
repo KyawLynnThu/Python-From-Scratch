03ae189898f8490aa507ca1e35af09edd0bad60e
Kyaw Linn Thu <67432058+KyawLynnThu@users.noreply.github.com>
2023-05-15 13:31:36 +0630

    boolean and operators

---
+++ boolean.py
@@ -0,0 +1,20 @@
+# hungry = False
+
+# print(hungry)
+
+# comparison operator > < >= <= ==
+
+# print(5>3)
+# print(5>=5)
+# print(3<=3)
+
+# logical operator (and or not)
+
+# print(not False)
+# print(not True)
+
+# print(5>1 and 5>3) #true
+# print(5>13 and 5>3) #false
+
+# print(5>1 or 3>4) #true
+# print(5<1 or 5<3) #false
