7a497a70fba5a520b8a3e683e184722a6857dee3
Kyaw Linn Thu <67432058+KyawLynnThu@users.noreply.github.com>
2023-05-15 15:37:28 +0630

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,22 @@
+person={
+    'name': 'Kyaw Linn Thu',
+    'age': 24
+}
+
+# add new data to person
+# person['hair_color']="black";
+
+# print(person)
+# print(person['name'])
+
+# print('name' in person) # OUTPUT: True // Check key name exist in person
+
+# if 'name' in person :
+#     print('work')
+
+# person_keys=list(person.keys()) # Get key lists
+# print(person_keys)
+
+person_value=list(person.values()) # Get value lists
+print(person_value)
+
