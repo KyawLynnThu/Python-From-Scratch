1894404b0d6daaac7a7751c7b13302846977a975
Kyaw Linn Thu <67432058+KyawLynnThu@users.noreply.github.com>
2023-05-16 11:54:01 +0630

    sorted_set

---
+++ sorted_set.py
@@ -0,0 +1,15 @@
+# nums=[2,3,4,5,6,7,1,4,5,7,9]
+# print(sorted(nums))
+
+# names=["name", "Ag Ag", "mg Mg", "Zaw", "Su", "Kyaw"]
+# print(sorted(names)) # it take priority Capital letter
+
+# nums=[2,3,4,5,6,7,1,4,5,7,9]
+# print(set(nums)) # takes one time for duplicate # used with for loop
+
+# for num in set(nums) :
+#     print(num)
+
+ages=[20,40,50,20]
+for age in set(ages) :
+    print(age)
