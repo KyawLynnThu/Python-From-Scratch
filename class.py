8c150f14fb91571d80ab642deb6c74fce11f1d43
Kyaw Linn Thu <67432058+KyawLynnThu@users.noreply.github.com>
2023-05-17 09:20:47 +0630

    class attribute and method

--- class.py
+++ class.py
@@ -3,16 +3,32 @@
 # print(type(name))
 
 class Car :
+    steeringWheel=1 #class level attribute || for same method in every methods
     def __init__(self, name, wheels) :
-        self.name=name
+        self.name=name #instance level attribute
         self.wheels=wheels
 
     def drive(self) :
         print(f'{self.name} is driving')
 
-lambo=Car("Lamborghnini", 4)
-# print(lambo.name)
-lambo.drive()
+    @classmethod
+    def common(cls) :
+        print(f'All car have only {cls.steeringWheel}')
 
-mercedes=Car("Mercedes", 4)
-mercedes.drive()
+# lambo=Car("Lamborghnini", 4)
+# # print(lambo.name)
+# lambo.drive()
+
+# print(lambo.steeringWheel) #class level attribute, method can use from object
+# print(lambo.common())
+
+# mercedes=Car("Mercedes", 4)
+# mercedes.drive()
+
+# instance level method
+# ---------------------
+#  Create object first. Run method from object
+
+# print(Car.steeringWheel)
+# Car.common()
+
